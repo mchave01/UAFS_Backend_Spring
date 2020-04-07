0cfa9ca150a78477e62422499d9fd301d21f5bca
mchave01 <mchave01@g.uafs.edu>
2020-04-06 20:58:12 -0500

    End of session

--- Backend_API/src/main/java/com/cap4/Backend_API/Classes.java
+++ Backend_API/src/main/java/com/cap4/Backend_API/Classes.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Table(name = "classes")
 @EntityListeners(AuditingEntityListener.class)
 public class Classes {
+    
     @Id
     private long classId;
     private String status;
